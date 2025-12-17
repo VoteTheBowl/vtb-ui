4dc4360c875e1057fc2313a9a39c6ac5eb1b04c3
Tyler Sheldon <tylerjsheldon@gmail.com>
2025-12-17 12:33:16 -0800

    Add ranked choice components (#8)
    * WIP
    
    * WIP
    
    * Add exhausted tally
    
    * Re-add dependency
    
    * Type fixes
    
    * Fix types.
    
    ---------
    
    Co-authored-by: Tyler Sheldon <tyler@Kiwi-Machine-II.hsd1.wa.comcast.net>
    Co-authored-by: Jesudas Joseph <jesudasmjoseph@gmail.com>

--- src/lib/voting-system/components/ranked-choice/RankedChoiceVoting.svelte
+++ src/lib/voting-system/components/ranked-choice/RankedChoiceVoting.svelte
@@ -1,1 +1,75 @@
-Not Implemented! Ranked Choice Voting Component Placeholder
+<script lang="ts">
+	import { dndzone, type DndEvent, type Item } from 'svelte-dnd-action';
+	import { flip } from 'svelte/animate';
+	import { getContext } from 'svelte';
+	import type { BallotContext } from '$lib/types';
+	import type { VotingComponentProps } from '$lib/voting-system/types';
+	import type { RankedSubmission } from './types';
+
+	let { event }: VotingComponentProps = $props();
+
+	const flipDurationMs = 200;
+
+	let ballotContext: BallotContext = getContext('ballot-data');
+
+	let items: Item[] = $state(
+		event.choices
+			.map((choiceStr) => {
+				return { id: choiceStr, title: choiceStr };
+			})
+			.sort(() => Math.random() - 0.5)
+	);
+
+	// svelte-ignore state_referenced_locally
+	ballotContext.submission = items.map((i) => i.id);
+	ballotContext.submissionIsValid = true;
+
+	function handleSort(e: CustomEvent<DndEvent>) {
+		items = e.detail.items;
+	}
+
+	function handleFinalize(e: CustomEvent<DndEvent>) {
+		items = e.detail.items;
+		ballotContext.submission = items.map((i) => i.id) as RankedSubmission;
+	}
+</script>
+
+<p>Drag your preferred options to the top.</p>
+
+<section
+	use:dndzone={{ items, flipDurationMs, dropTargetStyle: { outline: 'none' } }}
+	onconsider={handleSort}
+	onfinalize={handleFinalize}
+>
+	{#each items as item, index (item.id)}
+		<div class="choice-card" animate:flip={{ duration: flipDurationMs }}>
+			<div class="rank-number">
+				{index + 1}.
+			</div>
+			<div>
+				{item.title}
+			</div>
+		</div>
+	{/each}
+</section>
+
+<style>
+	.choice-card {
+		display: flex;
+		min-width: 15em;
+		background-color: #e4e4e4;
+		border-radius: 8px;
+		margin: 15px;
+		padding: 15px 20px;
+	}
+
+	.rank-number {
+		padding-right: 10px;
+		font-weight: bold;
+		color: #909090;
+	}
+
+	section {
+		min-height: 12em;
+	}
+</style>
