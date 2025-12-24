6f935cdc0937f7a34feb4d9c6e14e9e801afd9aa
Jesudas Joseph <jesudasmjoseph@gmail.com>
2025-12-23 22:07:55 -0500

    Fix home page typos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,9 @@
 <Heading tag="h1" class="mb-6">Vote The Bowl</Heading>
 
 <P>
-	Vote The Bowl is a webapp for organizing and participating in voting events. Create an event and
-	invite others to vote. You don't need an account just start by creating event below and sharing
-	the registration link via QR-code or a Link.
+	Make your cook off voting easy! Create an event and invite others to vote. You don't need an
+	account just start by creating an event below and sharing the registration link or QR-code. You
+	can choose the voting system that best suits your needs (more on the way).
 </P>
 
 <Hr />
