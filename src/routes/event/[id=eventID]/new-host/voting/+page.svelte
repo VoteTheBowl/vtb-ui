24363ebc75ca4bd54c1395abd1eb74ff4a34d969
Jesudas Joseph <jesudasmjoseph@gmail.com>
2026-02-21 20:12:06 -0800

    WIP

---
+++ src/routes/event/[id=eventID]/new-host/voting/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	import { Button } from 'flowbite-svelte';
+	import { ArrowRightOutline, ArrowLeftOutline } from 'flowbite-svelte-icons';
+
+	let eventID = $derived(Number(page.params.id));
+</script>
+
+<div class="flex w-full gap-6">
+	<Button
+		size="xl"
+		color="red"
+		onclick={() => {
+			goto(`/event/${eventID}/new-host/registration`);
+		}}
+	>
+		<ArrowLeftOutline class="me-2 h-5 w-5" /> Registration
+	</Button>
+	<Button
+		size="xl"
+		color="blue"
+		onclick={() => {
+			goto(`/event/${eventID}/new-host/results`);
+		}}
+	>
+		Close Voting <ArrowRightOutline class="ms-2 h-5 w-5" />
+	</Button>
+</div>
